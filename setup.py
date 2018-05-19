73b265f7dac4da3f6441e6fa58f2660533933406
Hugo Castilho <hcastilho@gmail.com>
2018-05-19 19:38:28 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
     name='nbgrader_merge',
     packages=find_packages(include=['nbgrader_merge']),
     url='https://github.com/hcastilho/nbgrader_merge',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
